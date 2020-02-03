90d5038407afb0c77f1ba5672d02e82366d9eb9b
andrius-butent <andrius.antipovas@gmail.com>
2020-02-03 15:10:20 +0200

    First unit tests for calculating distances

--- src/test/java/com/satalia/beer/BeerTestApplicationTests.java
+++ src/test/java/com/satalia/beer/BeerTestApplicationTests.java
@@ -1,13 +1,61 @@
 package com.satalia.beer;
 
+import com.satalia.beer.model.BreweryCodes;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BeerTestApplicationTests {
 
+	private static final double LATITUDE = 51.742503;
+	private static final double LONGITUDE = 19.432956;
+
 	@Test
-	void contextLoads() {
+	public void when_BothBreweryIsNullForCalculatingDistance_Expect_ZeroDistance() {
+		// given
+		BreweryCodes breweryA = null;
+		BreweryCodes breweryB = null;
+		Double expectedDistance = (double) 0;
+
+		// when
+		Double distance = BeerUtils.haversine(breweryA, breweryB);
+
+		//then
+		Assert.assertEquals(expectedDistance, distance);
 	}
 
+	@Test
+	public void when_OneBreweryIsNullForCalculatingDistance_Expect_ZeroDistance() {
+		// given
+		BreweryCodes breweryA = new BreweryCodes();
+		breweryA.setLatitude(LATITUDE);
+		breweryA.setLongitude(LONGITUDE);
+		BreweryCodes breweryB = null;
+		Double expectedDistance = (double) 0;
+
+		// when
+		Double distance = BeerUtils.haversine(breweryA, breweryB);
+
+		//then
+		Assert.assertEquals(expectedDistance, distance);
+	}
+
+	@Test
+	public void when_BreweriesIsNotNullForCalculatingDistance_Expect_PositiveDistance() {
+		// given
+		BreweryCodes breweryA = new BreweryCodes();
+		breweryA.setLatitude(LATITUDE);
+		breweryA.setLongitude(LONGITUDE);
+
+		BreweryCodes breweryB = new BreweryCodes();
+		breweryB.setLatitude(0d);
+		breweryB.setLongitude(0d);
+
+		// when
+		double distance = BeerUtils.haversine(breweryA, breweryB);
+
+		//then
+		Assert.assertTrue(distance > 0);
+	}
 }
