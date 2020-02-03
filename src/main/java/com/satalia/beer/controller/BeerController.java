438b1e54fe8315497edc71fc62538382e463ec53
andrius-butent <andrius.antipovas@gmail.com>
2020-02-03 14:34:51 +0200

    Implementation of main logic

--- src/main/java/com/satalia/beer/controller/BeerController.java
+++ src/main/java/com/satalia/beer/controller/BeerController.java
@@ -1,7 +1,10 @@
 package com.satalia.beer.controller;
 
 import com.satalia.beer.BeerUtils;
+import com.satalia.beer.model.Beers;
 import com.satalia.beer.model.BreweryCodes;
+import com.satalia.beer.model.TravelHistory;
+import com.satalia.beer.service.BeersService;
 import com.satalia.beer.service.BreweryCodesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/beer")
@@ -20,7 +22,10 @@ public class BeerController {
 	@Autowired
 	private BreweryCodesService breweryCodesService;
 
-	private static final int MAX_DISTANCE = 2000;
+	@Autowired
+	private BeersService beersService;
+
+	private List<Long> result = new ArrayList<>();
 
 	@RequestMapping("/form")
   public String getMainForm(@ModelAttribute("myCoordinates") BreweryCodes myCoordinates, Model model) {
@@ -33,18 +38,68 @@ public class BeerController {
 	public @ResponseBody String startBeerTravel(@ModelAttribute("myCoordinates") BreweryCodes myCoordinates) {
 
 		List<BreweryCodes> breweryList = new ArrayList<>();
-
-		myCoordinates.setId(0L);
-		breweryList.add(myCoordinates);
-
 		for (BreweryCodes brewery : breweryCodesService.getAllBreweryCodes()) {
 
 			// we calculate distance between home and breweries
-			double distance = BeerUtils.haversine(myCoordinates, brewery);
+			double distanceToHome = BeerUtils.haversine(myCoordinates, brewery);
 
 			// collecting breweries that fit by the distance
-			if (distance <= MAX_DISTANCE / 2d) {
+			if (distanceToHome <= BeerUtils.MAX_DISTANCE / 2d) {
 				breweryList.add(brewery);
+			}
+		}
+
+		// adding beers to their breweries
+		List<Beers> beers = beersService.getAllBeers();
+		for (BreweryCodes bc : breweryList) {
+			for (Beers beer : beers) {
+				if (beer.getBreweryId().equals(bc.getBreweryId())) {
+					bc.addBeer(beer);
+				}
+			}
+		}
+
+		int totalBeerCount = 0;
+
+		// creating initial travel point
+		TravelHistory home = new TravelHistory (myCoordinates.getLatitude(), myCoordinates.getLongitude(), 0d, 0);
+		home.setParents(Collections.singletonList(0L));
+
+		// stack for calculations
+		Stack<TravelHistory> stack = new Stack<>();
+		stack.add(home);
+
+		// the loop will work until all possible breweries will be visited
+		while (!stack.isEmpty()) {
+
+			TravelHistory currentPoint = stack.pop();
+			List<Long> breweryIds = currentPoint.getParents();
+
+			for (BreweryCodes nextBrewery : breweryList) {
+
+				if (breweryIds.contains(nextBrewery.getBreweryId())) {
+					continue;
+				}
+
+				double distanceToHome = BeerUtils.haversine(myCoordinates, nextBrewery);
+				double distanceBetween = BeerUtils.haversine(currentPoint, nextBrewery) + currentPoint.getTraveledDistance();
+
+				int beerCount = currentPoint.getCollectedBeers() + nextBrewery.getBeerCount();
+
+				if ((distanceBetween + distanceToHome) <= BeerUtils.MAX_DISTANCE) {
+
+					totalBeerCount = beerCount;
+
+					// new travel history point
+					TravelHistory travel = new TravelHistory(nextBrewery.getLatitude(), nextBrewery.getLongitude(),
+						distanceBetween, totalBeerCount);
+					travel.setParents(new ArrayList<>(currentPoint.getParents()));
+					travel.getParents().add(nextBrewery.getBreweryId());
+
+					stack.push(travel);
+
+					result = new ArrayList<>(travel.getParents());
+				}
 			}
 		}
 
