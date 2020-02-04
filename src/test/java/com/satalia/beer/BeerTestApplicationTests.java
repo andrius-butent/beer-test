3db8f5d20f1ca7f8edf38afcf040507c7e5c2154
andrius-butent <andrius.antipovas@gmail.com>
2020-02-04 22:16:51 +0200

    Unit tests for DB

--- src/test/java/com/satalia/beer/BeerTestApplicationTests.java
+++ src/test/java/com/satalia/beer/BeerTestApplicationTests.java
@@ -1,15 +1,32 @@
 package com.satalia.beer;
 
+import com.satalia.beer.model.Beers;
+import com.satalia.beer.model.Brewery;
 import com.satalia.beer.model.BreweryCodes;
+import com.satalia.beer.service.BeersService;
+import com.satalia.beer.service.BreweryCodesService;
+import com.satalia.beer.service.BreweryService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class BeerTestApplicationTests {
 
 	private static final double LATITUDE = 51.742503;
 	private static final double LONGITUDE = 19.432956;
+
+	@Autowired
+	private BreweryCodesService breweryCodesService;
+
+	@Autowired
+	private BeersService beersService;
+
+	@Autowired
+	private BreweryService breweryService;
 
 	@Test
 	public void when_BothBreweryIsNullForCalculatingDistance_Expect_ZeroDistance() {
@@ -57,5 +74,32 @@ class BeerTestApplicationTests {
 
 		//then
 		Assert.assertTrue(distance > 0);
+	}
+
+	@Test
+	public void when_BeersAreSelectingFromDB_Expect_NotEmptyList() {
+		// when
+		List<Beers> beerList = beersService.getAllBeers();
+
+		//then
+		Assert.assertTrue(beerList.size() > 0);
+	}
+
+	@Test
+	public void when_BreweriesAreSelectedFromDB_Expect_NotEmptyList() {
+		// when
+		List<Brewery> breweryList = breweryService.getAllBreweries();
+
+		//then
+		Assert.assertTrue(breweryList.size() > 0);
+	}
+
+	@Test
+	public void when_BreweryCodesAreSelectedFromDB_Expect_NotEmptyList() {
+		// when
+		List<BreweryCodes> codes = breweryCodesService.getAllBreweryCodes();
+
+		//then
+		Assert.assertTrue(codes.size() > 0);
 	}
 }
